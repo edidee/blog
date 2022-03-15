c9c32351e4341990aea091a1fc9b0b9a6017bee4
Edidee <etokedidionge@gmail.com>
2022-03-15 12:31:37 +0100

    fetch post data

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 
+function fetchPost() {
+  
+      
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Can't fetch data");
+        }
+        return response.json();
+      })
+      .then((json) => {
+          console.log(json);
+        const posts = json
+          .map((post) => {
+            return `
+              <div class="post">
+                    <h3 class="post-title">${post.title}</h3>
+                    <p>${post.body}</p>
+                    <a>Read more</a>
+                </div>
+              `;
+          })
+          .join("");
+        console.log(
+          posts
+        );
+        document.getElementById("post").insertAdjacentHTML("afterbegin", posts);
 
-async function fetchPost() {
-   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    console.log(response);
+      })
+    
+  .catch ((error) => {
+    console.log(error);
+  })
 }
+
+fetchPost();
+
+
+// async function fetchPost() {
+//     try {
+
+//         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+//          console.log(response);
+//     } catch (error){
+//         console.log(error);
+//     }
+// }
 
